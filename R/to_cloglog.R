87ff7ba348d4c512acd4112c996d512556c4a1c0
Dr John Baumgartner <MQ20145115@mq.edu.au>
2017-05-10 18:22:32 +1000

    Add transformation functions

---
+++ R/to_cloglog.R
@@ -0,0 +1,22 @@
+#' Transform raw or logistic values to cloglog values
+#'
+#' Transform Maxent raw or logistic values to cloglog (complementary log-log)
+#' values.
+#'
+#' @param x The values to transform. This can be, e.g., a numeric atomic vector,
+#'   numeric matrix or \code{Raster*} object.
+#' @param from A character string indicating the type of the source values. This
+#'   can be one of 'raw', 'cloglog', or 'logistic'.
+#' @param H The entropy of the Maxent distribution. This is only used when
+#'   \code{from='raw'}.
+#' @keywords maxent, lambdas
+#' @seealso \code{\link{to_logistic}}
+#' @export
+to_cloglog <- function(x, from, H) {
+  if(from=='raw' & missing(H)) 
+    stop('When from is "raw", H must not be missing.')
+  switch(from, 
+         'cloglog'=x,
+         'raw'=1-exp(-exp(H)*r),
+         'logistic'=1-exp(-(x/(1-x))))
+}
---
+++ R/to_logistic.R
@@ -0,0 +1,22 @@
+#' Transform raw or cloglog values to logistic values
+#'
+#' Transform Maxent raw or cloglog (complementary log-log) values to logistic
+#' values.
+#' 
+#' @param x The values to Transform. This can be, e.g., a numeric atomic vector,
+#'   numeric matrix or \code{Raster*} object.
+#' @param from A character string indicating the type of the source values. This
+#'   can be one of 'raw', 'cloglog', or 'logistic'.
+#' @param H The entropy of the Maxent distribution. This is only used when
+#'   \code{from='raw'}.
+#' @keywords maxent, lambdas
+#' @seealso \code{\link{to_cloglog}}
+#' @export
+to_logistic <- function(x, from, H) {
+  if(from=='raw' & missing(H)) 
+    stop('When from is "raw", H must not be missing.')
+  switch(from, 
+         'cloglog'=1/(log(1-x) - 1) + 1,
+         'raw'=(exp(H)*x)/(1+(exp(H)*x)),
+         'logistic'=x)
+}
